7d57585b446ca59b5f9614785bd19bf0776724f0
Vedprakash321 <131158289+Vedprakash321@users.noreply.github.com>
2023-09-24 18:11:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function calculateMinCost() {
   //your code here
-  
+  const ropeLengths = input.split(",").map((str) => parseInt(str.trim(), 10));
+  const minCost = findMinCost(ropeLengths);
+  return minCost;
   
   
 }  
